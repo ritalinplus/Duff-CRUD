c229f6a3928ebf4bde5595df07cab25db8900af0
Javier Abella <abeljvrg@gmail.com>
2019-08-27 15:47:45 +0200

    core.serializers: add absolute url to UserSerializer

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+
 from core.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     """Basic serializer for User Model"""
+    url = serializers.HyperlinkedIdentityField(
+        view_name="users-detail",
+    )
+
     class Meta:
         model = User
         fields = '__all__'
