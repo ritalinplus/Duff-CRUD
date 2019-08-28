d052e8d13e6bd32aa113e0520494b86710f73f55
Javier Abella <abeljvrg@gmail.com>
2019-08-28 13:06:11 +0200

    core.models: Update iban field to be unique

--- src/core/models.py
+++ src/core/models.py
@@ -5,7 +5,7 @@ class Client(models.Model):
     """Client model class"""
     name = models.CharField(max_length=100, help_text='Client first name')
     surname = models.CharField(max_length=100, help_text='Client surname')
-    iban = models.CharField(max_length=100, help_text='Client IBAN account')
+    iban = models.CharField(max_length=100, unique=True, help_text='Client IBAN account')
 
     def __str__(self):
         return f'{self.name} {self.surname} {self.iban}'
