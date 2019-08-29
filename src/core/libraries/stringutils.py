e6f039f6101293cf2d8f79126b6b559f3d6163d5
Javier Abella <abeljvrg@gmail.com>
2019-08-29 11:46:31 +0200

    core.libraries.stringutils: Add new method only_letters_and_blanks

--- src/core/libraries/stringutils.py
+++ src/core/libraries/stringutils.py
@@ -11,3 +11,24 @@ class StringUtils:
 
         """
         return value.isalpha()
+
+    @staticmethod
+    def only_letters_and_blanks(value):
+        """Allow only letters and blank spaces (but not only blank spaces) in value parameter.
+
+        Args:
+            value (str): data to check.
+
+        Return:
+            bool: True if string only has letters or blank spaces, False otherwise.
+
+        """
+        result = False
+
+        if value.isspace():
+            return result
+
+        elif all(x.isalpha() or x.isspace() for x in value):
+            result = True
+
+        return result
