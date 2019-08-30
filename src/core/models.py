13bf46b551a2a368aaf79f1fd8ed9f02db5572db
Javier Abella <abeljvrg@gmail.com>
2019-08-30 13:51:59 +0200

    core.models: overwrite Client model save method to lowercase name and suname

--- src/core/models.py
+++ src/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class OwnerModel(models.Model):
+    """Owner model class"""
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
@@ -17,6 +18,13 @@ class Client(OwnerModel):
 
     def __str__(self):
         return f'{self.name} {self.surname} {self.iban}'
+
+    def save(self, *args, **kwargs):
+        """Saves client attributes converting to lowercase name and surname"""
+        self.name = self.name.lower()
+        self.surname = self.surname.lower()
+
+        return super(Client, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "Clients"
