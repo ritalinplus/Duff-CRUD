25f4038c341f1ebd5a2bb70eb8b3e046d36e5cdf
Javier Abella <abeljvrg@gmail.com>
2019-09-02 17:05:14 +0200

    rindcrud: update DIRS setting

--- src/rinduscrud/settings.py
+++ src/rinduscrud/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'rinduscrud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, os.path.join(BASE_DIR, 'core', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
