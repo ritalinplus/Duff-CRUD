99dd9fed8a206918346054d7d4d980475fb5f83d
Javier Abella <abeljvrg@gmail.com>
2019-09-03 14:52:34 +0200

    core.urls: add swagger utl

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,10 +1,19 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
 
 from core.apiviews import ClientViewSet
 
+
+schema_view = get_swagger_view(title='Duff CRUD API')
 
 version = 'v1'
 router = DefaultRouter()
 router.register(f'{version}/clients', ClientViewSet, base_name='clients')
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path(f'{version}/swagger-docs/', schema_view)
+]
+
+urlpatterns += router.urls
